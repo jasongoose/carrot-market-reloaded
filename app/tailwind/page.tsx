1bb4b7fe593d6749b2985ca2ae02af9364751c39
jasongoose <ssjk7789@gmail.com>
2024-06-08 16:45:11 +0900

    tailwindcss로 Card 컴포넌트 만들기

---
+++ app/tailwind/page.tsx
@@ -0,0 +1,9 @@
+import Card from "@/components/card";
+
+export default function Page() {
+  return (
+    <main className="flex items-center justify-center h-screen p-5 bg-gray-300">
+       <Card />
+    </main>
+  )
+}

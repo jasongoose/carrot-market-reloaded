8217e8f18e4ca333e65f2a03fc6b58e4f14d93d9
jasongoose <ssjk7789@gmail.com>
2024-06-08 18:04:15 +0900

    SearchInput 컴포넌트 생성
    - ring class를 적용
    - tailwind에는 css variable에 값을 지정하는 class와 그 변수를 이용하여 동적으로 스타일을 정의하는 class가 있음😳

--- app/tailwind/page.tsx
+++ app/tailwind/page.tsx
@@ -1,9 +1,11 @@
 import Card from "@/components/card";
+import SearchInput from "@/components/search-input";
 
 export default function Page() {
   return (
     <main className="flex items-center justify-center h-screen p-5 bg-gray-100 dark:bg-gray-700">
-       <Card />
+       {/* <Card /> */}
+       <SearchInput />
     </main>
   )
 }

e31ef46401e0ff5585454f40074092132b9030a3
jasongoose <ssjk7789@gmail.com>
2024-06-09 09:42:46 +0900

    invalid modifier 적용

--- components/search-input.tsx
+++ components/search-input.tsx
@@ -2,7 +2,7 @@ export default function SearchInput() {
   return (
     <div className="w-full max-w-screen-sm">
       <div className="flex flex-col gap-2 p-5 bg-white shadow-xl rounded-3xl dark:bg-gray-600 md:flex-row">
-        <input className="w-full h-12 pl-5 transition-shadow bg-gray-200 rounded-full outline-none ring ring-transparent focus:ring-orange-500 focus:ring-offset-2 placeholder:drop-shadow" type="text" placeholder="Search here"/>
+        <input type="email" placeholder="Search here" required className="w-full h-12 pl-5 transition-shadow bg-gray-200 rounded-full outline-none ring ring-transparent focus:ring-orange-500 focus:ring-offset-2 placeholder:drop-shadow invalid:bg-red-100"/>
         <button className="py-2 font-medium text-white transition-transform bg-black rounded-full outline-none md:px-10 active:scale-90 focus:scale-9 bg-gradient-to-tr from-cyan-500 via-yellow-400 to-purple-400">Search</button>
       </div>
     </div>

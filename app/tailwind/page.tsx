cf5f57dd06e2692ad424cac5113759172d5e471f
jasongoose <ssjk7789@gmail.com>
2024-06-09 16:55:22 +0900

    animate-spin 적용

--- app/tailwind/page.tsx
+++ app/tailwind/page.tsx
@@ -4,7 +4,7 @@ import SearchInput from "@/components/search-input";
 
 export default function Page() {
   return (
-    <main className="flex h-screen flex-col items-center justify-center gap-5 bg-gray-100 p-5 sm:bg-red-100 md:bg-green-100 lg:bg-cyan-100 xl:bg-orange-100 dark:bg-gray-700">
+    <main className="flex flex-col items-center justify-center gap-5 bg-gray-100 p-5 sm:bg-red-100 md:bg-green-100 lg:bg-cyan-100 xl:bg-orange-100 dark:bg-gray-700">
       <Card />
       <SearchInput />
       <List
--- components/list-item.tsx
+++ components/list-item.tsx
@@ -7,9 +7,11 @@ export default function ListItem({ person, index }: Props) {
   return (
     <li className="flex items-center gap-5 rounded-xl border-b-2 p-2.5 first:border-t-0 first:pt-0 last:border-b-0 odd:bg-gray-100 even:bg-orange-100 last:md:pb-0">
       <div className="size-7 rounded-full bg-blue-400"></div>
-      <span className="text-lg font-medium">{person}</span>
-      <div className="flex size-5 items-center justify-center rounded-full bg-red-500 text-white">
-        {index}
+      {/* <span className="text-lg font-medium">{person}</span> */}
+      <div className="h-4 w-40 rounded-full bg-gray-400"></div>
+      <div className="h-4 w-20 rounded-full bg-gray-400"></div>
+      <div className="flex size-5 animate-spin items-center justify-center rounded-full bg-red-500 text-white">
+        ⏳
       </div>
     </li>
   );

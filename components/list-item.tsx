79972b5a2ef6294382f4d8540b2f875375916ca5
jasongoose <ssjk7789@gmail.com>
2024-06-09 17:01:14 +0900

    animate-ping 적용

--- components/list-item.tsx
+++ components/list-item.tsx
@@ -5,14 +5,13 @@ export interface Props {
 
 export default function ListItem({ person, index }: Props) {
   return (
-    <li className="flex items-center gap-5 rounded-xl border-b-2 p-2.5 *:animate-pulse first:border-t-0 first:pt-0 last:border-b-0 odd:bg-gray-100 even:bg-orange-100 last:md:pb-0">
+    <li className="flex items-center gap-5 rounded-xl border-b-2 p-2.5 first:border-t-0 first:pt-0 last:border-b-0 odd:bg-gray-100 even:bg-orange-100 last:md:pb-0">
       <div className="size-7 rounded-full bg-blue-400"></div>
-      {/* <span className="text-lg font-medium">{person}</span> */}
-      <div className="h-4 w-40 rounded-full bg-gray-400"></div>
-      <div className="h-4 w-20 rounded-full bg-gray-400"></div>
-      {/* <div className="flex size-5 animate-spin items-center justify-center rounded-full bg-red-500 text-white">
-        ⏳
-      </div> */}
+      <span className="text-lg font-medium">{person}</span>
+      <div className="relative flex size-6 items-center justify-center rounded-full bg-red-500 text-white">
+        <span className="z-10">{index}</span>
+        <div className="absolute size-6 animate-ping rounded-full bg-red-500"></div>
+      </div>
     </li>
   );
 }

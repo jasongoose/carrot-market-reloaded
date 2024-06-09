c8f8967275eec617293c1158fe2080788cf20e9c
jasongoose <ssjk7789@gmail.com>
2024-06-09 16:22:31 +0900

    odd, even modifier 적용

--- components/list-item.tsx
+++ components/list-item.tsx
@@ -5,7 +5,7 @@ export interface Props {
 
 export default function ListItem({ person, index }: Props) {
   return (
-    <li className="flex items-center gap-5">
+    <li className="flex items-center gap-5 rounded-xl p-2.5 odd:bg-gray-100 even:bg-cyan-100">
       <div className="size-7 rounded-full bg-blue-400"></div>
       <span className="text-lg font-medium">{person}</span>
       <div className="flex size-5 items-center justify-center rounded-full bg-red-500 text-white">

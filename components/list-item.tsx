a63257b9d53648f26da3c456e81f5f6a30d90844
jasongoose <ssjk7789@gmail.com>
2024-06-09 17:05:50 +0900

    empty modifier 적용

--- components/list-item.tsx
+++ components/list-item.tsx
@@ -7,7 +7,9 @@ export default function ListItem({ person, index }: Props) {
   return (
     <li className="flex items-center gap-5 rounded-xl border-b-2 p-2.5 first:border-t-0 first:pt-0 last:border-b-0 odd:bg-gray-100 even:bg-orange-100 last:md:pb-0">
       <div className="size-7 rounded-full bg-blue-400"></div>
-      <span className="text-lg font-medium">{person}</span>
+      <span className="text-lg font-medium empty:h-5 empty:w-24 empty:animate-pulse empty:rounded-full empty:bg-gray-300">
+        {person}
+      </span>
       <div className="relative flex size-6 items-center justify-center rounded-full bg-red-500 text-white">
         <span className="z-10">{index}</span>
         <div className="absolute size-6 animate-ping rounded-full bg-red-500"></div>

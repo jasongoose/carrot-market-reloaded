79a385b74fa00f261a049e96ab408bc09c13d886
jasongoose <ssjk7789@gmail.com>
2024-06-09 09:23:06 +0900

    responsive modifier(break point) 적용

--- app/tailwind/page.tsx
+++ app/tailwind/page.tsx
@@ -3,7 +3,7 @@ import SearchInput from "@/components/search-input";
 
 export default function Page() {
   return (
-    <main className="flex items-center justify-center h-screen p-5 bg-gray-100 dark:bg-gray-700">
+    <main className="flex items-center justify-center h-screen p-5 bg-gray-100 dark:bg-gray-700 sm:bg-red-100  md:bg-green-100 lg:bg-cyan-100 xl:bg-orange-100">
        {/* <Card /> */}
        <SearchInput />
     </main>

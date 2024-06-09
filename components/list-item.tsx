4118f111ac173266f5bd3f0913763410179033d4
jasongoose <ssjk7789@gmail.com>
2024-06-09 17:17:24 +0900

    group modifier 적용

--- components/list-item.tsx
+++ components/list-item.tsx
@@ -5,9 +5,9 @@ export interface Props {
 
 export default function ListItem({ person, index }: Props) {
   return (
-    <li className="flex items-center gap-5 rounded-xl border-b-2 p-2.5 first:border-t-0 first:pt-0 last:border-b-0 odd:bg-gray-100 even:bg-orange-100 last:md:pb-0">
+    <li className="group flex items-center gap-5 rounded-xl border-b-2 p-2.5 first:border-t-0 first:pt-0 last:border-b-0 odd:bg-gray-100 even:bg-orange-100 last:md:pb-0">
       <div className="size-7 rounded-full bg-blue-400"></div>
-      <span className="text-lg font-medium empty:h-5 empty:w-24 empty:animate-pulse empty:rounded-full empty:bg-gray-300">
+      <span className="text-lg font-medium empty:h-5 empty:w-24 empty:animate-pulse empty:rounded-full empty:bg-gray-300 group-hover:text-red-500">
         {person}
       </span>
       <div className="relative flex size-6 items-center justify-center rounded-full bg-red-500 text-white">
--- components/search-input.tsx
+++ components/search-input.tsx
@@ -1,11 +1,20 @@
 export default function SearchInput() {
   return (
     <div className="w-full max-w-screen-sm">
-      <div className="flex flex-col gap-2 p-5 bg-white shadow-xl rounded-3xl dark:bg-gray-600 md:flex-row *:outline-none has-[:invalid]:ring has-[:invalid]:ring-blue-300 ring ring-transparent transition-shadow">
-        <input type="text" placeholder="Email Address" required className="w-full h-12 pl-5 transition-shadow bg-gray-200 rounded-full ring ring-transparent focus:ring-green-500 focus:ring-offset-2 placeholder:drop-shadow invalid:focus:ring-red-500 peer"/>
-        <span className="hidden font-medium text-red-500 peer-invalid:block">Email is required</span>
-        <button className="py-2 font-medium text-white transition-transform bg-black rounded-full md:px-10 active:scale-90 focus:scale-9">Login</button>
+      <div className="group flex flex-col gap-2 rounded-3xl bg-white p-5 shadow-xl ring ring-transparent transition-shadow *:outline-none has-[:invalid]:ring has-[:invalid]:ring-blue-300 md:flex-row dark:bg-gray-600">
+        <input
+          type="text"
+          placeholder="Email Address"
+          required
+          className="peer h-12 w-full rounded-full bg-gray-200 pl-5 ring ring-transparent transition-shadow placeholder:drop-shadow focus:ring-green-500 focus:ring-offset-2 invalid:focus:ring-red-500"
+        />
+        <span className="hidden font-medium text-red-500 group-focus-within:block">
+          Email is required
+        </span>
+        <button className="focus:scale-9 rounded-full bg-black py-2 font-medium text-white transition-transform active:scale-90 md:px-10">
+          Login
+        </button>
       </div>
     </div>
-  )
+  );
 }

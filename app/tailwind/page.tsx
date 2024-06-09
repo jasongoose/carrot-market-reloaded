b9dd25dc0238506425c8a2402a92104e2ff45d6f
jasongoose <ssjk7789@gmail.com>
2024-06-09 17:54:40 +0900

    jit compiler 학습 + arbitrary value 적용

--- app/tailwind/page.tsx
+++ app/tailwind/page.tsx
@@ -5,7 +5,7 @@ import SearchInput from "@/components/search-input";
 export default function Page() {
   return (
     <main className="flex flex-col items-center justify-center gap-5 bg-gray-100 p-5 sm:bg-red-100 md:bg-green-100 lg:bg-cyan-100 xl:bg-orange-100 dark:bg-gray-700">
-      <Card />
+      {/* <Card />
       <SearchInput />
       <List
         personList={[
@@ -20,7 +20,10 @@ export default function Page() {
           "Marco",
           "",
         ]}
-      />
+      /> */}
+      <button className="h-[492.4px] rounded-[11.11px] bg-[#543cb8]">
+        Testing...
+      </button>
     </main>
   );
 }

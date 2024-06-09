6f7203586bd06f8a210e67cf95aaa03afe13f947
jasongoose <ssjk7789@gmail.com>
2024-06-09 09:46:40 +0900

    중첩 modifier 적용

--- components/search-input.tsx
+++ components/search-input.tsx
@@ -2,7 +2,7 @@ export default function SearchInput() {
   return (
     <div className="w-full max-w-screen-sm">
       <div className="flex flex-col gap-2 p-5 bg-white shadow-xl rounded-3xl dark:bg-gray-600 md:flex-row">
-        <input type="email" placeholder="Search here" required className="w-full h-12 pl-5 transition-shadow bg-gray-200 rounded-full outline-none ring ring-transparent focus:ring-orange-500 focus:ring-offset-2 placeholder:drop-shadow invalid:bg-red-100"/>
+        <input type="email" placeholder="Search here" required className="w-full h-12 pl-5 transition-shadow bg-gray-200 rounded-full outline-none ring ring-transparent focus:ring-green-500 focus:ring-offset-2 placeholder:drop-shadow invalid:focus:ring-red-500"/>
         <button className="py-2 font-medium text-white transition-transform bg-black rounded-full outline-none md:px-10 active:scale-90 focus:scale-9 bg-gradient-to-tr from-cyan-500 via-yellow-400 to-purple-400">Search</button>
       </div>
     </div>
